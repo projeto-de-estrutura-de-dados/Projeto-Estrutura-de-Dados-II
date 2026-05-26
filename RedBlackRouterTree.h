09c0dec75ef2a26febf23b8462dd036bed558d37
vyctor_gabriel11 <vyctorgmachado@gmail.com>
2026-05-26 13:53:26 -0300

    Update RedBlackRouterTree.h
    Implementa rotacoes da Red-Black

--- RedBlackRouterTree.h
+++ RedBlackRouterTree.h
@@ -32,6 +32,60 @@ private:
     Node* root = nullptr;
     int rotations = 0;
 
+    // ROTACAO PARA ESQUERDA
+    Node* rotateLeft(Node* x) {
+
+        rotations++;
+
+        Node* y = x->right;
+
+        x->right = y->left;
+
+        if (y->left != nullptr)
+            y->left->parent = x;
+
+        y->parent = x->parent;
+
+        if (x->parent == nullptr)
+            root = y;
+        else if (x == x->parent->left)
+            x->parent->left = y;
+        else
+            x->parent->right = y;
+
+        y->left = x;
+        x->parent = y;
+
+        return y;
+    }
+
+    // ROTACAO PARA DIREITA
+    Node* rotateRight(Node* y) {
+
+        rotations++;
+
+        Node* x = y->left;
+
+        y->left = x->right;
+
+        if (x->right != nullptr)
+            x->right->parent = y;
+
+        x->parent = y->parent;
+
+        if (y->parent == nullptr)
+            root = x;
+        else if (y == y->parent->left)
+            y->parent->left = x;
+        else
+            y->parent->right = x;
+
+        x->right = y;
+        y->parent = x;
+
+        return x;
+    }
+
 public:
 
     int getRotations() {

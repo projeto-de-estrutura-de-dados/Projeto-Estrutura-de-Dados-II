4f8c33db98b0507671852de292d2a7b8b56f3661
Mardson Varela Lima <105571646+PiedroCraft1379@users.noreply.github.com>
2026-05-28 20:36:00 -0300

    feat: adiciona inserção na árvore Red-Black, benchmarking de busca/deleção na AVL e registro de resultados em CSV no Benchmark.cpp

--- Benchmark.cpp
+++ Benchmark.cpp
@@ -36,4 +36,39 @@ int main() {
       chrono::duration_cast<chrono::nanoseconds>(endAVL - startAVL).count();
 
   cout << "AVL Insert Time:" << avlInsertTime << " ns" << endl;
+
+  auto startRB = chrono::high_resolution_clock::now();
+
+  for (auto &rule : rules) {
+    rb.insert(rule);
+  }
+
+  auto endRB = chrono::high_resolution_clock::now();
+
+  auto searchStart = chrono::high_resolution_clock::now();
+
+  for (int i = 1; i <= 100000; i++) {
+    avl.search(i);
+  }
+
+  auto searchEnd = chrono::high_resolution_clock::now();
+
+  int deleteAmount = 100000 * 0.2;
+
+  for (int i = 1; i <= deleteAmount; i++) {
+    avl.deleteRule(i);
+  }
+
+  avl.validateAVL();
+  rb.validateRedBlack();
+
+  cout << "AVL Height: " << avl.height() << endl;
+
+  cout << "AVL Rotations: " << avl.getRotations() << endl;
+
+  ofstream file("results.csv");
+  file << "Estrutura,Tempo\n";
+  file << "AVL," << avlInsertTime << "\n";
+
+  file << "RedBlack," << rbInsertTime << "\n";
 }

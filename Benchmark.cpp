102d3d2cc8c10b07b98bd8d8ec10d29368b02800
Mardson Varela Lima <105571646+PiedroCraft1379@users.noreply.github.com>
2026-05-28 17:45:53 -0300

    feat: Adição de script de benchmark para comparar o desempenho de árvores AVL e rubro-negras.

---
+++ Benchmark.cpp
@@ -0,0 +1,27 @@
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+
+#include "AVLRouterTree.h"
+#include "RedBlackRouterTree.h"
+
+using namespace std;
+
+int main() {
+
+  vector<PacketRule> rules;
+
+  for (int i = 1; i <= 100000; i++) {
+
+    rules.push_back(PacketRule(i, "192.168.0.1", "10.0.0.1", rand() % 100));
+  }
+  AVLRouterTree avl;
+  RedBlackRouterTree rb;
+
+  auto start = chrono::high_resolution_clock::now();
+  auto end = chrono::high_resolution_clock::now();
+  auto duration =
+      chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+}
